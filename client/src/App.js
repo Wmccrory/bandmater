40c0deb1f7cfcdf64f503bf0d2b4a21dfbd82e7f
Will <wmccrory@gmail.com>
2018-04-15 18:15:12 -0500

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+
+import Landing from "./pages/landing/Landing";
+const Dashboard = () => {return <h1>Dashboard</h1>};
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>	
+					<div className="container">
+						<Route exact path="/" component={Landing} />
+						<Route exact path="/dashboard" component={Dashboard} />
+					</div>
+				</BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/pages/landing/Landing.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./landing.css";
+
+const Landing = () => {
+	return (
+		<div className="landing">
+			<h1>Bandmater</h1>
+			<h2>Connecting bands, fans, and venues</h2>
+		</div>
+	)
+}
+
+export default Landing;
